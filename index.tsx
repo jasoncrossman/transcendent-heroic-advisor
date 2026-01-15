374cf2db806a6f688dd92b569059f982e2273e5d
jasoncrossman <147454348+jasoncrossman@users.noreply.github.com>
2026-01-15 09:18:42 -0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App';
-import PurchasePage from './PurchasePage';
-import FreeResources from './FreeResources';
-import AcademyPage from './pages/AcademyPage'; // This tells the map to find your new styled file
+import PurchasePage from './pages/PurchasePage';
+import FreeResources from './pages/FreeResources';
+import AcademyPage from './pages/AcademyPage';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
-  createRoot(rootElement).render(
+  const root = createRoot(rootElement);
+  root.render(
     <React.StrictMode>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/purchase" element={<PurchasePage />} />
           <Route path="/freeresources" element={<FreeResources />} />
-          {/* This uses the styled page you just created in the pages folder */}
           <Route path="/academy" element={<AcademyPage />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
