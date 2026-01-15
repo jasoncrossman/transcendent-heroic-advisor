ca36d9b02291dec775946320e96aa6a94f52a662
jasoncrossman <147454348+jasoncrossman@users.noreply.github.com>
2026-01-15 09:44:14 -0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,16 +1,16 @@
+
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-// We are intentionally NOT importing the pages yet to isolate the crash
 const rootElement = document.getElementById('root');
-
-if (rootElement) {
-  const root = createRoot(rootElement);
-  root.render(
-    <div style={{ padding: '50px', fontFamily: 'sans-serif', textAlign: 'center' }}>
-      <h1>System Check: ONLINE</h1>
-      <p>If you can see this, the website wiring is perfect.</p>
-      <p>The crash is happening inside one of the Page files.</p>
-    </div>
-  );
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
 }
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
