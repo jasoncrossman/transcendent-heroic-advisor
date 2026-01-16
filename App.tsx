83557bbbc8cb6822a0d56b2b218a9c16ca3f51de
jasoncrossman <147454348+jasoncrossman@users.noreply.github.com>
2026-01-15 23:57:37 -0800

    Add Academy and Congratulations pages to routing

--- App.tsx
+++ App.tsx
@@ -3,7 +3,8 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage.tsx';
 import PurchasePage from './pages/PurchasePage.tsx';
 import FreeResourcesPage from './pages/FreeResourcesPage.tsx';
-import AcademyPage from './pages/AcademyPage.tsx'; // ADDED
+import AcademyPage from './pages/AcademyPage.tsx';
+import CongratulationsPage from './pages/CongratulationsPage.tsx'; // THE ONLY NEW IMPORT
 import Header from './components/Header.tsx';
 import Footer from './components/Footer.tsx';
 
@@ -17,7 +18,8 @@ const App: React.FC = () => {
             <Route path="/" element={<LandingPage />} />
             <Route path="/purchase" element={<PurchasePage />} />
             <Route path="/free-resources" element={<FreeResourcesPage />} />
-            <Route path="/academy" element={<AcademyPage />} /> {/* ADDED */}
+            <Route path="/academy" element={<AcademyPage />} />
+            <Route path="/congratulations" element={<CongratulationsPage />} /> {/* THE ONLY NEW ROUTE */}
           </Routes>
         </main>
         <Footer />
